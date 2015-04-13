eedb60992c74dbe24e2e64a21b3e0b8469c0f52a
rimahina <rimahina@hotmail.com>
2015-04-13 18:30:55 +0200

    Js separado

--- js/scroll.js
+++ js/scroll.js
@@ -1,59 +1,58 @@
-// JavaScript Document
-               
-	<script>			   
+
+	   
 var controller = new ScrollMagic.Controller();
+var controller2 = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+
+//primer avión
 	          
-TweenMax.set("#animate2", {left: "-=75%"});
-						
-var tween = new TimelineMax()
-	.to("#animate2", 3, {top: "+=300",left: "+=145%",});
-														
-							
-var scene = new ScrollMagic.Scene({triggerElement: "#trigger2"})
+TweenMax.set("#avion", {left: "-=40%", top: "+=30", opacity:0});				
+var tween = new TweenMax.to("#avion", 3, {top: "+=300",left: "+=80%",opacity:1,ease:Circ.easeOut});	
+																			
+var scene = new ScrollMagic.Scene({triggerElement: "#lanzaavion1"})
 	.setTween(tween)
-	.addIndicators({name: "timeline"}) // add indicators (requires plugin)
 	.addTo(controller);
 
 //para las fotos
-var tween = new TweenMax.staggerTo([fotos1, fotos2, fotos3, fotos4, fotos5, fotos6], 1, {scale:0.2, opacity:0.3}, 0.25); //para darle un nomre distinto a cada elemento...
+	TweenMax.staggerTo([fotos1, fotos2, fotos3, fotos4, fotos5, fotos6], 1, {scale:0.1, opacity:0.1}, 0);
+	var tween = new TweenMax.staggerTo([fotos1, fotos2, fotos3, fotos4, fotos5, fotos6], 1, {scale:1, opacity:1}, 0.25); //para darle un nomre distinto a cada elemento...
 	
 	var scene = new ScrollMagic.Scene({triggerElement: "#animafotos"})
 	.setTween(tween)
 	.addTo(controller);										
 
-// set start offset
-	TweenMax.set("#animate3", {left: "+=75%"});
-// build tween
-	var tween = new TimelineMax()
-		.to("#animate3", 4, {top: "+=250",left: "-=145%",});
-								
+//primer avión
+	          
+TweenMax.set("#avion2", {left: "+=40%", top: "-=30", opacity:0});				
+var tween = new TweenMax.to("#avion2", 3, {top: "-=300",left: "-=80%", opacity:1,ease:Circ.easeOut});	
+																			
+var scene = new ScrollMagic.Scene({triggerElement: "#lanzaavion2"})
+	.setTween(tween)
+	.addTo(controller);
 	
-	var scene = new ScrollMagic.Scene({triggerElement: "#trigger3"})
-				.setTween(tween)
-				.addIndicators({name: "timeline"}) // add indicators (requires plugin)
-				.addTo(controller);
+	
+//ultima foto
 
-
-					
-
-
-						// init controller
-	var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
-
+TweenMax.set("#final", {opacity:0});				
+var tween = new TweenMax.to("#final", 3, {opacity:1,ease:Circ.easeOut});	
+																			
+var scene = new ScrollMagic.Scene({triggerElement: "#ultimafoto"})
+	.setTween(tween)
+	.addTo(controller);
+	
+//Fotos borrosas
 	// build scenes
 	new ScrollMagic.Scene({triggerElement: "#parallax1"})
-					.setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
-					.addTo(controller);
+					.setTween("#parallax1 > div", {y: "100%", ease: Linear.easeNone})
+					.addTo(controller2);
 
 	new ScrollMagic.Scene({triggerElement: "#parallax2"})
-					.setTween("#parallax2 > div", {y: "80%", ease: Linear.easeNone})
-					.addTo(controller);
+					.setTween("#parallax2 > div", {y: "100%", ease: Linear.easeNone})
+					.addTo(controller2);
 
 	new ScrollMagic.Scene({triggerElement: "#parallax3"})
-					.setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
-					.addTo(controller);
+					.setTween("#parallax3 > div", {y: "100%", ease: Linear.easeNone})
+					.addTo(controller2);
 					
 	new ScrollMagic.Scene({triggerElement: "#parallax4"})
-					.setTween("#parallax4 > div", {y: "80%", ease: Linear.easeNone})
-					.addTo(controller);
-					</script>
+					.setTween("#parallax4 > div", {y: "100%", ease: Linear.easeNone})
+					.addTo(controller2);
